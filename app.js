8682c47a744b49f716e789be38c99ed79bd25be4
HankSWillians <germansrivas@gmail.com>
2026-04-23 22:24:01 +0000

    feat: usar PORT dinamico para Render

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ function sumar(a, b) {
 }
 
 const http = require('http');
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   const resultado = sumar(2, 3);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -10,7 +12,7 @@ const server = http.createServer((req, res) => {
 });
 
 if (require.main === module) {
-  server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+  server.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
 }
 
 module.exports = { sumar };
